6602d6c27ef1221cc271b0084795eb75d8f50ed8
Neetu <147698613+Neetu264326@users.noreply.github.com>
2026-04-03 13:01:36 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+    let map={};
+    for(let i=0;i<nums.length;i++){
+        let comp=target-nums[i];
+        if(map.hasOwnProperty(comp)){
+        return [map[comp],i];
+    }
+    map[nums[i]]=i;
+}
+}  }
